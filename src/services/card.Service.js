7f43b388cfd9439103c665bf6f141ebdee945355
LautyM22 <franco.mellado@est.fi.uncoma.edu.ar>
2026-06-07 19:53:57 -0300

    Se colocan las funciones de create, delete and update en card.service

--- src/services/card.Service.js
+++ src/services/card.Service.js
@@ -32,12 +32,9 @@ export async function getAllCards(page, limit, lang) {
   };
 }
 
-// Obtiene una carta específica por ID
-// ...existing code...
-
 export async function getCardById(id, lang) {
   const card = await prisma.card.findUnique({
-    where: { id: parseInt(id) },  // Cambiar id por parseInt(id)
+    where: { id: parseInt(id) },
     include: {
       translations: true,
       type: {
@@ -58,4 +55,69 @@ export async function getCardById(id, lang) {
   }
 
   return mapCardToLang(card, lang);
+}
+
+export async function createCard(body) {
+  const card = await prisma.card.create({
+    data: {
+      cost: body.cost,
+      atk: body.atk,
+      def: body.def,
+      image: body.image,
+      typeId: body.typeId,
+      rarityId: body.rarityId,
+      translations: {
+        create: body.translations,
+      },
+    },
+    include: {
+      translations: true,
+      type: { include: { translations: true } },
+      rarity: { include: { translations: true } },
+    },
+  });
+
+  return mapCardToLang(card, 'es');
+}
+
+export async function updateCard(id, body) {
+  const card = await prisma.card.findUnique({ where: { id: parseInt(id) } });
+  
+  if (!card) {
+    return null;
+  }
+
+  const updatedCard = await prisma.card.update({
+    where: { id: parseInt(id) },
+    data: {
+      cost: body.cost,
+      atk: body.atk,
+      def: body.def,
+      image: body.image,
+      typeId: body.typeId,
+      rarityId: body.rarityId,
+      translations: {
+        deleteMany: {},
+        create: body.translations,
+      },
+    },
+    include: {
+      translations: true,
+      type: { include: { translations: true } },
+      rarity: { include: { translations: true } },
+    },
+  });
+
+  return mapCardToLang(updatedCard, 'es');
+}
+
+export async function deleteCard(id) {
+  const card = await prisma.card.findUnique({ where: { id: parseInt(id) } });
+  
+  if (!card) {
+    return false;
+  }
+
+  await prisma.card.delete({ where: { id: parseInt(id) } });
+  return true;
 }
